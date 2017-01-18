9ff6c6445939f53079c1adf2b403db8fc0265494
preetiahuja <preeti.umesh.ahuja@one.verizon.com>
2017-01-17 16:32:06 -0800

    Use one api for public and private feed, send results based on req authentication

--- app/routes/v1/a/feed.js
+++ app/routes/v1/a/feed.js
@@ -8,8 +8,12 @@ var service = require('../../../service')
 
 function getFeed(req, res) {
 	utils.l.d("Get feed request for user: " + JSON.stringify(req.user)
-		+ " with console type: " + req.param('consoleType'))
-	service.feedService.getFeed(req.user, req.param('consoleType'), false, req.param('myEvents'), function(err, feed) {
+		+ " with console type: " + req.param('consoleType') + " . User is authenticated = " + req.isAuthenticated())
+	var isAuthenticated = req.isAuthenticated()
+	var user = isAuthenticated ? req.user : null
+	var consoleType = isAuthenticated ? req.param('consoleType') : null
+	var myEvents = isAuthenticated ? req.param('myEvents') : null
+	service.feedService.getFeed(user, consoleType, !isAuthenticated, myEvents, function(err, feed) {
 		if (err) {
 			routeUtils.handleAPIError(req, res, err, err, {utm_dnt:"feed"})
 		} else {
@@ -29,5 +33,5 @@ function publicFeed(req, res) {
 }
 
 routeUtils.rGet(router, '/get', 'getFeed', getFeed, {utm_dnt:"feed"})
-routeUtils.rGet(router, '/public', 'publicFeed', publicFeed, {utm_dnt:"publicFeed"})
+//routeUtils.rGet(router, '/public', 'publicFeed', publicFeed, {utm_dnt:"publicFeed"})
 module.exports = router
